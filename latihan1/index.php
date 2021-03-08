885995883e301e26c6ea61e906cf2d61c15be0ba
Dewa019 <79686851+Dewa019@users.noreply.github.com>
2021-03-08 13:01:37 +0800

    Add files via upload

---
+++ latihan1/index.php
@@ -0,0 +1,63 @@
+vh<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Selamat Datang di Website Dewa Yogik</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Menghasilkan lulusan yang berkualitas dan berdaya saing tinggi dalam bidang TIK seperti programer, administrator basis data, teknisi jaringan, multimedia.">
+    <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+    <link rel="canonical" href="http://informatika.undiksha.ac.id/">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="">
+    <meta property="og:title" content="">
+    <meta property="og:description" content="">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+</head>
+
+<body>
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">MANFAAT GYM</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">Home<span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">about</a>
+                <a class="nav-item nav-link" href="#">Biodata Diri </a>
+                <a class="nav-item nav-link" href="media/form.html">Registers</a>
+            </div>
+        </div>
+    </nav>
+    <div class="jumbotron text-center">
+        <h1>manfaat gym untuk tubuh</h1>
+        <img src="media\gym.jpg">
+        
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h3>Meningkatkan percaya diri</h3>
+                <p>Penurunan berat badan, otot yang semakin terbentuk dan lemak tubuh yang semakin terkikis merupakan manfaat fitness di gym yang dapat Anda peroleh. Perubahan bentuk tubuh ini tentu dapat membuat penampilan Anda terlihat lebih menarik sehingga Anda dapat tampil lebih percaya diri.</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Memelihara kesehatan mental</h3>
+                <p>Manfaat fitness tidak hanya untuk kesehatan fisik saja, tetapi juga baik untuk kesehatan mental. Sebab, dengan melakukan latihan fisik, hormon serotonin yang terdapat di otak akan mengalami peningkatan, sehingga mampu membuat kinerja otak atau kejernihan mental mengalami peningkatan. Fitness sebagai pilihan olahraga juga dapat membantu mengurangi stres. Hal ini dapat bermanfaat terhadap kinerja seseorang di tempat bekerja. Seseorang yang aktif melakukan aktivitas fisik dan olahraga, cenderung lebih produktif di tempat kerja.</p>
+            </div>
+            <div class="col-sm-4">
+                <h3>Terhindar dari berbagai macam penyakit</h3>
+                <p>Aktivitas fisik seperti fitness yang dilakukan secara rutin mampu mengurangi risiko terkena berbagai penyakit, seperti penyakit jantung koroner, diabetes tipe 2, kanker usus besar, kanker payudara, depresi, demensia, dan osteoarthritis. Sebuah studi menunjukkan bahwa wanita berusia 45 tahun ke atas yang kerap melakukan aktivitas fisik, seperti berjalan cepat, lebih dari dua jam dalam seminggu memiliki risiko 33% lebih kecil untuk terkena stroke dibandingkan mereka yang tidak melakukannya.</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
